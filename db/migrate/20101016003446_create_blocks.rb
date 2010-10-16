37f4fc0773021ee320ac8051181c41751202c65e
Edmund Lam <elam@realestate.com.au>
2010-10-16 12:07:19 +1100

    update db

--- db/migrate/20101016003446_create_blocks.rb
+++ db/migrate/20101016003446_create_blocks.rb
@@ -1,12 +1,13 @@
 class CreateBlocks < ActiveRecord::Migration
   def self.up
     create_table :blocks do |t|
+      t.integer, :user_id
       t.integer, :x
       t.integer, :y
-      t.string, :north
-      t.string, :east
-      t.string, :south
-      t.string :west
+      t.integer, :north
+      t.integer, :east
+      t.integer, :south
+      t.integer :west
 
       t.timestamps
     end
