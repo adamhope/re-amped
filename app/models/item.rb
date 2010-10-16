9a241d66cc44bfbb6bf02fa67cbfbc6a5bfd7187
Mike Breeze <mike.breeze@rea-group.com>
2010-10-16 18:09:20 +1100

    Added search method on items: http://localhost:3000/items/search/toys.json

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,4 +24,27 @@ class Item < ActiveRecord::Base
 
     create().update_attributes(:powerhouse_id => id, :image_url => img_url)
   end
+
+  # http://api.powerhousemuseum.com/api/v1/item/json/?api_key=4db781fa8bf0ad9&description=helmets
+  def self.search_description description
+    search_results = []
+    base_url = "http://api.powerhousemuseum.com"
+    query = "&description=#{description}"
+    items_response = HTTParty.get("#{base_url}/api/v1/item/json/?api_key=4db781fa8bf0ad9#{query}")
+    return items_response if items_response.nil?
+    items_response.as_json["items"].each do |powerhouse_item|
+      multimedia_uri = base_url + powerhouse_item["multimedia_uri"]
+      multimedia = HTTParty.get(multimedia_uri)
+      unless multimedia.as_json["multimedia"].empty?
+        img_url = multimedia.as_json["multimedia"][0]["images"]["thumbnail"]["url"]
+        unless img_url.nil?
+          # This is an item with an image. Add it to our DB.
+          create().update_attributes(:powerhouse_id => id, :image_url => img_url)
+          search_results.push({"powerhouse_id" => powerhouse_item.id, "image_url" => img_url})
+        end
+      end
+    end
+    search_results.as_json
+  end
+
 end
